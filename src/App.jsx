9bc93d2cc5f53a0acc3ce86cd712fb170bcf5050
abhinavyadav1806 <abhinav1806yadav@gmail.com>
2023-12-18 09:21:42 +0530

    s4

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./component/Header/Header.jsx";
+import CoreConcepts from "./component/CoreConcepts.jsx";
+import Examples from "./component/Examples.jsx";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <CoreConcepts />
+        <Examples />
+      </main>
+    </>
+  );
+}
+
+export default App;
