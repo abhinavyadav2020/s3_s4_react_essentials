2750a69e5bb1446fb39e3a9d9830139775522bbb
abhinavyadav2020 <abhinav1806yadav@gmail.com>
2023-12-18 13:49:21 +0530

    ...props in TabButton

--- src/component/TabButton.jsx
+++ src/component/TabButton.jsx
@@ -1,6 +1,8 @@
+// export default function TabButton({ children, isSelected, onSelect }) {
 export default function TabButton({ children, isSelected, ...props }) {
   return (
     <li>
+      {/* <button className={isSelected ? "active" : undefined} onClick={onSelect}>{children}</button> */}
       <button className={isSelected ? "active" : undefined} {...props}>{children}</button>
     </li>
   );
